0211bdf17c4342a9ec7ba5216521d0aeddb496cd
Hisham <hishambarakat16@gmail.com>
2024-06-25 22:37:47 +0800

    Updated dataflow/utils.py and added jittor

--- tensorlayerx/dataflow/utils.py
+++ tensorlayerx/dataflow/utils.py
@@ -157,6 +157,58 @@ def default_collate_torch(batch):
     raise TypeError(default_collate_err_msg_format.format(elem_type))
 
 
+def default_collate_jittor(batch):
+    import jittor
+    import jittor.transform
+    elem = batch[0]
+    elem_type = type(elem)
+    if isinstance(elem, jittor.Var):
+        out = None
+
+        return jittor.stack(batch, 0, out=out)
+    elif elem_type.__module__ == 'numpy' and elem_type.__name__ != 'str_' \
+            and elem_type.__name__ != 'string_':
+        if elem_type.__name__ == 'ndarray' or elem_type.__name__ == 'memmap':
+            # array of string classes and object
+            if np_str_obj_array_pattern.search(elem.dtype.str) is not None:
+                raise TypeError(default_collate_err_msg_format.format(elem.dtype))
+
+            return default_collate([jittor.transform.to_tensor(b) for b in batch])
+        elif elem.shape == ():  # scalars
+            return jittor.transform.to_tensor(batch)
+    elif isinstance(elem, float):
+        return jittor.transform.to_tensor(batch, dtype=jittor.float64)
+    elif isinstance(elem, int):
+        return jittor.transform.to_tensor(batch)
+    elif isinstance(elem, string_classes):
+        return batch
+    elif isinstance(elem, collections.abc.Mapping):
+        try:
+            return elem_type({key: default_collate([d[key] for d in batch]) for key in elem})
+        except TypeError:
+            # The mapping type may not support `__init__(iterable)`.
+            return {key: default_collate([d[key] for d in batch]) for key in elem}
+    elif isinstance(elem, tuple) and hasattr(elem, '_fields'):  # namedtuple
+        return elem_type(*(default_collate(samples) for samples in zip(*batch)))
+    elif isinstance(elem, collections.abc.Sequence):
+        # check to make sure that the elements in batch have consistent size
+        it = iter(batch)
+        elem_size = len(next(it))
+        if not all(len(elem) == elem_size for elem in it):
+            raise RuntimeError('each element in list of batch should be of equal size')
+        transposed = list(zip(*batch))  # It may be accessed twice, so we use a list.
+
+        if isinstance(elem, tuple):
+            return [default_collate(samples) for samples in transposed]  # Backwards compatibility.
+        else:
+            try:
+                return elem_type([default_collate(samples) for samples in transposed])
+            except TypeError:
+                # The sequence type may not support `__init__(iterable)` (e.g., `range`).
+                return [default_collate(samples) for samples in transposed]
+
+    raise TypeError(default_collate_err_msg_format.format(elem_type))
+
 def default_collate_paddle(batch):
     import paddle
     elem = batch[0]
@@ -310,6 +362,8 @@ def default_collate(batch):
         return default_collate_ms(batch)
     elif BACKEND == 'oneflow':
         return default_collate_flow(batch)
+    elif BACKEND == 'jittor':
+        return default_collate_jittor(batch)
 
 
 class _DatasetKind(object):
